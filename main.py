1d83325332afe1c774bb165f7c1171494745631a
Darya _V_13 <118071419+VoronovaDA@users.noreply.github.com>
2023-02-21 11:35:44 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
 import psycopg2
-from psycopg2 import Error
 
 
-try:
-    conn = psycopg2.connect(user="postgres", 
-                            password="7404",
-                            database="homework_db")
-    cur = conn.cursor()
+with psycopg2.connect(database="homework_db", user="postgres", password="7404") as conn:
+    with conn.cursor() as cur:
+        cur.execute("""
+                    DROP TABLE Phones;
+                    DROP TABLE Client;
+                    """)
 
-    cur.execute("""
-                DROP TABLE Phones;
-                DROP TABLE Client;
-                """)
 
     def create_db(cur):
+        with conn.cursor() as cur:
             cur.execute("""
                 CREATE TABLE IF NOT EXISTS Client(
                 client_id SERIAL PRIMARY KEY,
@@ -32,6 +29,7 @@ try:
 
                 
     def add_client(cur, first_name, last_name, email):
+        with conn.cursor() as cur:
             cur.execute("""
                 INSERT INTO Client(first_name, last_name, email)
                 VALUES(%s, %s, %s)
@@ -42,7 +40,8 @@ try:
             return cur.fetchone()
             
         
-    def add_phone(cur, client_id, phone):           
+    def add_phone(cur, client_id, phone):
+        with conn.cursor() as cur:           
             cur.execute("""
                 INSERT INTO Phones(client_id, phone)
                 VALUES(%s, %s)
@@ -53,19 +52,34 @@ try:
             return cur.fetchone()
 
         
-    def change_client(cur, first_name, last_name, email, client_id):          
+    def change_client(cur, first_name, last_name, client_id):
+        with conn.cursor() as cur:          
             cur.execute("""
                 UPDATE Client
-                SET first_name=%s, last_name=%s, email=%s 
+                SET first_name=%s AND last_name=%s  
                 WHERE client_id=%s
                 RETURNING client_id, first_name, last_name, email;
-                """, (first_name, last_name, email, client_id, ))           
+                """, (first_name, last_name, client_id, ))           
+            print("Записи обновлены!")
+            print(cur.fetchall())
+            return cur.fetchall()
+    
+
+    def change_email(cur, email, client_id):
+        with conn.cursor() as cur:          
+            cur.execute("""
+                UPDATE Client
+                SET first_name=%s OR last_name=%s OR email=%s 
+                WHERE client_id=%s
+                RETURNING client_id, first_name, last_name, email;
+                """, (email, client_id, ))           
             print("Записи обновлены!")
             print(cur.fetchall())
             return cur.fetchall()
        
     
-    def change_phone(cur, client_id, phone):          
+    def change_phone(cur, client_id, phone):
+        with conn.cursor() as cur:          
             cur.execute("""
                 UPDATE Phones
                 SET phone=%s
@@ -77,17 +91,19 @@ try:
             return cur.fetchall()
         
     
-    def find_client(cur, first_name, last_name, email, phone):           
+    def find_client(cur, first_name, last_name, email, phone):
+        with conn.cursor() as cur:           
             cur.execute("""
                 SELECT c.first_name, c.last_name, c.email, p.phone FROM Client c
                 LEFT JOIN Phones p ON c.client_id = p.client_id
-                WHERE c.first_name=%s OR c.last_name=%s OR c.email=%s OR p.phone=%s;
+                WHERE c.first_name=%s AND c.last_name=%s AND c.email=%s AND p.phone=%s;
                 """, (first_name, last_name, email, phone, ))
             print(cur.fetchall())
             return cur.fetchall()
 
 
     def delete_phone(cur, client_id, phone):
+        with conn.cursor() as cur:
             cur.execute("""
                 DELETE FROM Phones
                 WHERE client_id=%s and phone=%s 
@@ -98,7 +114,8 @@ try:
             return cur.fetchall()
 
   
-    def delete_client(cur, client_id):            
+    def delete_client(cur, client_id):
+        with conn.cursor() as cur:            
             cur.execute("""
                 DELETE FROM Client
                 WHERE client_id=%s;
@@ -107,6 +124,7 @@ try:
 
 
     def select(cur):
+        with conn.cursor() as cur:
             cur.execute("""
                 SELECT * FROM Client;
                 """)
@@ -117,16 +135,19 @@ try:
             print(cur.fetchall())
 
 
-    print('Создать таблицы "1" ') 
-    print('Добавить клиента "2"') 
-    print('Добавить номер телефона клиента "3" ') 
-    print('Обновить данные клиента "4" ') 
-    print('Обновить номер телефона клиента "5" ') 
-    print('Найти клиента по его данным и номеру телефона "6" ')
-    print('Удалить номер телефона клиета "7" ') 
-    print('Удалить клиента "8" ') 
-    print('Вывести данные таблиц "9"')
-    print('Выйти из программы "10"') 
+    print()
+    print('Здравствуйте! Какую функцию надо выполнить?')
+    print()
+    print('"1" Создать таблицы') 
+    print('"2" Добавить клиента') 
+    print('"3" Добавить номер телефона клиента') 
+    print('"4" Обновить данные клиента')  
+    print('"5" Найти клиента по его данным')
+    print('"6" Удалить номер телефона клиета') 
+    print('"7" Удалить клиента') 
+    print('"8" Вывести данные таблиц')
+    print('"9" Выйти из программы') 
+    print()
     
     while True:
         function = input('Введите нужную команду: ')
@@ -142,31 +163,42 @@ try:
             phone = input('Введите телефон клиента: ')   
             add_phone(cur, client_id, phone)               
         elif function == '4':
+            print()
+            print('Какие данные надо изменить?')
+            print('"1" Имя и фамилию клиента')
+            print('"2" Email клиента')
+            print('"3" Телефон клиента')
+            task = input('Введите нужную команду: ')
             client_id = input('Введите id клиента: ')
-            first_name = input('Введите новое имя клиента: ')
-            last_name = input('Введите новую фамилию клиента: ')
-            email = input('Введите новый Email клиента: ')
-            change_client(cur, first_name, last_name, email, client_id)
+            if task == "1":
+                first_name = input('Введите новое имя клиента: ')
+                last_name = input('Введите новую фамилию клиента: ')
+                change_client(cur, first_name, last_name, client_id)
+            elif task == "2":
+                email = input('Введите новый Email клиента: ')
+                change_email(cur, email, client_id)
+            elif task == "3":
+                phone = input('Введите новый телефон клиента: ')
+                change_phone(cur, phone, client_id)
+            else:
+                print('Введена неверная команда!')
+                break
         elif function == '5':
-            client_id = input('Введите id клиента: ')  
-            phone = input('Введите новый телефон клиента: ')
-            change_phone(cur, phone, client_id)
-        elif function == '6':
             first_name = input('Введите имя клиента: ')
             last_name = input('Введите фамилию клиента: ')
             email = input('Введите Email клиента: ')
             phone = input('Введите телефон клиента: ')
             find_client(cur, first_name, last_name, email, phone)
-        elif function == '7':
+        elif function == '6':
             client_id = input('Введите id клиента: ')
             phone = input('Введите телефон клиента, который надо удалить: ')
             delete_phone(cur, client_id, phone)
-        elif function == '8':
+        elif function == '7':
             client_id = input('Введите id клиента: ')
             delete_client(cur, client_id)
-        elif function == '9':
+        elif function == '8':
             select(cur)   
-        elif function == '10':
+        elif function == '9':
             if conn:
                 cur.close()
                 conn.close()
@@ -175,5 +207,3 @@ try:
         else:
             print('Введена неверная команда!')
             break
-except (Exception, Error) as error:
-    print("Ошибка при работе с PostgreSQL", error)
